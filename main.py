c30f6f5e27300c54c9638823f71fabad694deed7
naidukarthi2193 <naidukarthi2193@gmail.com>
2020-08-30 14:09:14 +0000

    lgdirectory added

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
 
 from typing import List
-
+import pandas as pd
 import uvicorn
 from fastapi.responses import HTMLResponse
-
 from fastapi import Depends, FastAPI, HTTPException ,Request
 from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session
-# from starlette.responses import RedirectResponse
-
 import models, schemas, crud
 from database import SessionLocal, engine
 from fastapi.templating import Jinja2Templates
@@ -26,6 +23,9 @@ app.add_middleware(
     allow_headers=["*"],
     allow_credentials=True,
 )
+
+villages = pd.read_csv("cleaned_villages.csv")
+block = pd.read_csv("blocks.csv",sep=";")
 
 def get_db():
     try:
@@ -64,6 +64,58 @@ def check_farmer(aadhar: str ,db : Session = Depends(get_db)):
         raise HTTPException(status_code=400 , detail="Farmer already registered")
     else:
         return { "status" : "Not Registed"}
+
+@app.get("/districts")
+def get_districts():
+    village_code = sorted(list(set(villages["District Code"])))
+    village_name = sorted(list(set(villages["District Name (In English)"])))
+    response = list()
+    for i in range(len(village_code)):
+        temp = dict()
+        temp['code'] = village_code[i]
+        temp['name'] = village_name[i]
+        response.append(temp)
+    return response
+
+@app.get("/subdistrict/{dis_code}")
+def get_subdistricts(dis_code):
+    temp_village = villages.loc[villages["District Code"] == int(dis_code)]
+
+    village_code = sorted(list(set(temp_village["Sub-District Code"])))
+    village_name = sorted(list(set(temp_village["Sub-District Name (In English)"])))
+    response = list()
+    for i in range(len(village_code)):
+        temp = dict()
+        temp['code'] = village_code[i]
+        temp['name'] = village_name[i]
+        response.append(temp)
+    return response
+
+@app.get("/village/{dis_code}")
+def get_villages(dis_code):
+    temp_village = villages.loc[villages["Sub-District Code"] == int(dis_code)]
+    village_code = sorted(list(set(temp_village["Village Code"])))
+    village_name = sorted(list(set(temp_village["Village Name (In Englsih)"])))
+    response = list()
+    for i in range(len(village_code)):
+        temp = dict()
+        temp['code'] = village_code[i]
+        temp['name'] = village_name[i]
+        response.append(temp)
+    return response
+
+@app.get("/block/{dis_code}")
+def get_blocks(dis_code):
+    temp_village = block.loc[block["District Code"] == int(dis_code)]
+    village_code = sorted(list(set(temp_village["Block Code"])))
+    village_name = sorted(list(set(temp_village["Block Name                      (In English)"])))
+    response = list()
+    for i in range(len(village_code)):
+        temp = dict()
+        temp['code'] = village_code[i]
+        temp['name'] = village_name[i]
+        response.append(temp)
+    return response
 
 if __name__ == "__main__":
     uvicorn.run(app, host='0.0.0.0')
